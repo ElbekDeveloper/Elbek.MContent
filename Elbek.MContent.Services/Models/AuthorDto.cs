d6321e7099c1e3ed5b2c5dcdc3de5f3fc17ee591
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-11 19:08:42 +0500

    Add data transfer objects

---
+++ Elbek.MContent.Services/Models/AuthorDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Elbek.MContent.Services.Models
+{
+    public class AuthorDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Elbek.MContent.Services/Models/MContentResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elbek.MContent.Services.Models
+{
+    public class MContentResult<T>
+    {
+        public int StatusCode { get; set; }
+        public List<string> Errors { get; set; }
+        public T Data { get; set; }
+    }
+}
