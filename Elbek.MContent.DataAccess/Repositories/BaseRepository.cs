04f31d681008797fc4989d55a6fa1eed719f2a36
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-11 15:52:55 +0500

    Create repositories

---
+++ Elbek.MContent.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using Elbek.MContent.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elbek.MContent.DataAccess.Repositories
+{
+    public class BaseRepository<TModel> : IRepository<TModel> where TModel : BaseEntity
+    {
+        public IQueryable<TModel> GenerateQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModel> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Elbek.MContent.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using Elbek.MContent.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elbek.MContent.DataAccess.Repositories
+{
+    public interface IRepository<TModel> where TModel : BaseEntity
+    {
+        IQueryable<TModel> GenerateQuery();
+        Task<TModel> GetById(Guid id);
+        Task<IEnumerable<TModel>> GetAll();
+    }
+}
