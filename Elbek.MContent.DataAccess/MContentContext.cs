4655888d55cf8c18ba2496356da729b2138e066a
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-11 15:53:07 +0500

    Add db context

---
+++ Elbek.MContent.DataAccess/MContentContext.cs
@@ -0,0 +1,14 @@
+﻿using Elbek.MContent.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Elbek.MContent.DataAccess
+{
+    public class MContentContext : DbContext
+    {
+        public MContentContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
