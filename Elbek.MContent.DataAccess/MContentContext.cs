a00c43cdda669232d53badec4ea56618cd491181
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-11 16:17:35 +0500

    Add FluentApi for model validation

--- Elbek.MContent.DataAccess/MContentContext.cs
+++ Elbek.MContent.DataAccess/MContentContext.cs
@@ -10,5 +10,24 @@ namespace Elbek.MContent.DataAccess
         {
         }
         public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>()
+                .Property(a => a.Id)
+                .HasColumnName("id")
+                .HasMaxLength(36)
+                .HasColumnType("varchar")
+                .IsRequired(true);
+
+            modelBuilder.Entity<Author>()
+                .Property(a => a.Name)
+                .HasColumnName("Name")
+                .HasMaxLength(50)
+                .HasColumnType("nvarchar")
+                .IsRequired(true);
+
+
+        }
     }
 }
