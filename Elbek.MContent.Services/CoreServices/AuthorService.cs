a20e7e48902cd77c48b20d3dbaad18eadd32e99b
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-12 15:02:43 +0500

    Create author services

---
+++ Elbek.MContent.Services/CoreServices/AuthorService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Elbek.MContent.DataAccess.Repositories;
+using Elbek.MContent.Services.Exceptions;
+using Elbek.MContent.Services.Models;
+using Elbek.MContent.Services.ValidationServices.AuthorValidator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Elbek.MContent.Services.CoreServices
+{
+    public interface IAuthorService
+    {
+        Task<IEnumerable<AuthorDto>> GetAuthorsAsync();
+        Task<AuthorDto> GetAuthorByIdAsync(Guid id);
+    }
+    public class AuthorService : IAuthorService
+    {
+        private readonly IAuthorRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly IAuthorValidationService _validationService;
+        public AuthorService(IAuthorRepository repository, IMapper mapper, IAuthorValidationService validationService)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _validationService = validationService;
+        }
+
+        public async Task<AuthorDto> GetAuthorByIdAsync(Guid id)
+        {
+            //Validate
+            var validationResult = _validationService.Validate(id);
+            if (validationResult.IsValid == false)
+            {
+                throw new ValidationException(validationResult.Errors, validationResult.StatusCode);
+            }
+            //Retrive 
+            var author = await _repository.GetByIdAsync(id);
+            if (author == null)
+            {
+               throw new VersionNotFoundException($"Author with '{id}' was not found");
+            }
+            return _mapper.Map<AuthorDto>(author);
+        }
+
+        public async Task<IEnumerable<AuthorDto>> GetAuthorsAsync()
+        {
+            var authors = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<AuthorDto>>(authors);
+        }
+    }
+}
---
+++ Elbek.MContent.Services/HostServices/AuthorsHostService.cs
@@ -0,0 +1,38 @@
+﻿using Elbek.MContent.Services.CoreServices;
+using Elbek.MContent.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Elbek.MContent.Services.HostServices
+{
+    [ApiController]
+    [Route("/api/Authors")]
+    public class AuthorsHostService : ControllerBase
+    {
+        private readonly IAuthorService _service;
+
+        public AuthorsHostService(IAuthorService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "All Authors", Type = typeof(MContentResult<IEnumerable<AuthorDto>>))]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult<MContentResult<AuthorDto>>> GetAuthors()
+        {
+            return Ok(await _service.GetAuthorsAsync());
+        }
+        [HttpGet]
+        [Route("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Author by Id", Type = typeof(MContentResult<IEnumerable<AuthorDto>>))]
+        [SwaggerResponse((int)HttpStatusCode.InternalServerError)]
+        public async Task<ActionResult<MContentResult<AuthorDto>>> GetAuthorById([FromRoute]Guid id)
+        {
+            return Ok(await _service.GetAuthorByIdAsync(id));
+        }
+    }
+}
