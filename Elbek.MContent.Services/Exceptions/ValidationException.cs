a5eb0cfed30e3eff15e06442bf2ea7d208e2b4d3
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-12 15:02:28 +0500

    Implement custom validation exception

---
+++ Elbek.MContent.Services/Exceptions/ValidationException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Elbek.MContent.Services.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public int StatusCode;
+        public List<String> ValidationErrors { get; set; }
+
+        public ValidationException(List<String> validationErrors, int statusCode = 400)
+        {
+            ValidationErrors = validationErrors;
+            StatusCode = statusCode;
+        }
+
+    }
+}
