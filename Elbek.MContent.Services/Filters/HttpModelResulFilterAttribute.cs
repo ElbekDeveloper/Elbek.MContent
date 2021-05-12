4fd1cc5e265a908d7588c362a8e77182b0919b9a
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-12 15:02:54 +0500

    Write custom filters

---
+++ Elbek.MContent.Services/Filters/HttpModelResulFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Elbek.MContent.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Elbek.MContent.Services.Filters
+{
+    public class HttpModelResultFilterAttribute: Attribute, IAsyncResultFilter
+    {
+        public async Task OnResultExecutionAsync(ResultExecutingContext context,
+            ResultExecutionDelegate next)
+        {
+            var result = context.Result as ObjectResult;
+            if(result !=  null)
+                result.Value = new MContentResult<object>()
+                {
+                    Data = result.Value, 
+                    Errors = new List<string>(), 
+                    StatusCode = (int)result.StatusCode
+                };
+            await next();
+        }
+    }
+}
