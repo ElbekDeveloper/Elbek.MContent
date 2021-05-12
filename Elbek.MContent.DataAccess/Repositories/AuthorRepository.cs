4807b882e052dabb95a9fe23436f11e133008385
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-12 15:00:17 +0500

    Create GetAuthors and GetAuthorById API methods

---
+++ Elbek.MContent.DataAccess/Repositories/AuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using Elbek.MContent.DataAccess.Data;
+
+namespace Elbek.MContent.DataAccess.Repositories
+{
+    public interface IAuthorRepository : IRepository<Author>
+    {
+
+    }
+    public class AuthorRepository : BaseRepository<Author>, IAuthorRepository
+    {
+        public AuthorRepository(MContentContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
