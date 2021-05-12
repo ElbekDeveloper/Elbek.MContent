cc32cf1b2b4aa2a464a14b05dea4cd3ce1941184
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-12 16:25:38 +0500

    Implement search by name

--- Elbek.MContent.DataAccess/Repositories/AuthorRepository.cs
+++ Elbek.MContent.DataAccess/Repositories/AuthorRepository.cs
@@ -1,15 +1,23 @@
 ﻿using Elbek.MContent.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Elbek.MContent.DataAccess.Repositories
 {
     public interface IAuthorRepository : IRepository<Author>
     {
-
+        Task<Author> GetAuthorByName(string name);
     }
     public class AuthorRepository : BaseRepository<Author>, IAuthorRepository
     {
         public AuthorRepository(MContentContext dbContext) : base(dbContext)
         {
+        }
+
+        public async Task<Author> GetAuthorByName(string name)
+        {
+           return await GenerateQuery().SingleOrDefaultAsync(i => i.Name == name);
         }
     }
 }
