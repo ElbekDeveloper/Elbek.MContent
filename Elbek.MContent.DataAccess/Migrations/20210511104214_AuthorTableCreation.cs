ecd3c1d4196ed1a53d34fc5b831ee0dae8060582
Restyled.io <commits@restyled.io>
2021-05-13 19:19:57 +0000

    Restyled by clang-format

--- Elbek.MContent.DataAccess/Migrations/20210511104214_AuthorTableCreation.cs
+++ Elbek.MContent.DataAccess/Migrations/20210511104214_AuthorTableCreation.cs
@@ -1,32 +1,28 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Elbek.MContent.DataAccess.Migrations
-{
-///TODO 4
-/// миграции тут нам не нужны, с базой работает проект Database, он должен создавать базу и заполнять ее данными
+namespace Elbek.MContent.DataAccess.Migrations {
+  /// TODO 4
+  /// миграции тут нам не нужны, с базой работает проект Database, он должен
+  /// создавать базу и заполнять ее данными
 
-public partial class AuthorTableCreation : Migration
-{
-    protected override void Up(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.CreateTable(
-            name: "Authors",
-            columns: table => new
-        {
-            Id = table.Column<Guid>(type: "varchar(36)", nullable: false),
-            Name = table.Column<string>(type: "nvarchar(50)", nullable: false)
-        },
-        constraints: table =>
-        {
-            table.PrimaryKey("PK_Authors", x => x.Id);
-        });
+  public partial class AuthorTableCreation : Migration {
+    protected override void Up(MigrationBuilder migrationBuilder) {
+      migrationBuilder.CreateTable(
+          name
+          : "Authors", columns
+          : table => new {Id = table.Column<Guid>(type
+                                                  : "varchar(36)", nullable
+                                                  : false),
+                          Name = table.Column<string>(type
+                                                      : "nvarchar(50)", nullable
+                                                      : false)},
+            constraints
+          : table => { table.PrimaryKey("PK_Authors", x => x.Id); });
     }
 
-    protected override void Down(MigrationBuilder migrationBuilder)
-    {
-        migrationBuilder.DropTable(
-            name: "Authors");
+    protected override void Down(MigrationBuilder migrationBuilder) {
+      migrationBuilder.DropTable(name : "Authors");
     }
-}
+  }
 }
--- Elbek.MContent.DataAccess/Repositories/AuthorRepository.cs
+++ Elbek.MContent.DataAccess/Repositories/AuthorRepository.cs
@@ -3,22 +3,17 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Elbek.MContent.DataAccess.Repositories
-{
-public interface IAuthorRepository : IRepository<Author>
-{
+namespace Elbek.MContent.DataAccess.Repositories {
+  public interface IAuthorRepository : IRepository<Author> {
     Task<Author> GetAuthorByName(string name);
-}
-public class AuthorRepository : BaseRepository<Author>, IAuthorRepository
-{
-    public AuthorRepository(MContentContext dbContext) : base(dbContext)
-    {
-    }
+  }
+  public class AuthorRepository : BaseRepository<Author>, IAuthorRepository {
+    public AuthorRepository(MContentContext dbContext) : base(dbContext) {}
 
-    public async Task<Author> GetAuthorByName(string name)
-    {
-        /// TODO 7 добавь .AsNoTracking(), обычно все гет операции(данные которых ты потом не будешь изменять мы не трекаем)
-        return await GenerateQuery().SingleOrDefaultAsync(i => i.Name == name);
+    public async Task<Author> GetAuthorByName(string name) {
+      /// TODO 7 добавь .AsNoTracking(), обычно все гет операции(данные которых
+      /// ты потом не будешь изменять мы не трекаем)
+      return await GenerateQuery().SingleOrDefaultAsync(i => i.Name == name);
     }
-}
+  }
 }
