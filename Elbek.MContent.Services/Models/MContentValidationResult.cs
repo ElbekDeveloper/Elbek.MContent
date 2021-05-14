142746d49a965159f0e717332d64740b06c4cd58
Luzhynski, Artsem (US - Chicago) <aluzhynski@deloitte.com>
2021-05-14 18:32:42 +0300

    Code review 2, 14 may

--- Elbek.MContent.Services/Models/MContentValidationResult.cs
+++ Elbek.MContent.Services/Models/MContentValidationResult.cs
@@ -8,6 +8,8 @@ namespace Elbek.MContent.Services.Models
 {
     public class MContentValidationResult
     {
+        ///TODO 2.2 хардкодить 400 не нужно, что если у тебя будет другой невалидный код, например 404
+        /// поле _statusCode здесь тоже не нужно
         private int _statusCode = 400;
         public bool IsValid { get; set; }
         public int StatusCode
@@ -23,6 +25,8 @@ namespace Elbek.MContent.Services.Models
         }
         public List<string> Errors { get; set; } = new List<string>();
 
+        ///TODO 2.1 это логика свойства IsValid.
+        /// UpdateIsValidFlagOnError - удалить, логику реализовать в get свойства IsValid
         public bool UpdateIsValidFlagOnError()
         {
             if (Errors.Any() == false)
