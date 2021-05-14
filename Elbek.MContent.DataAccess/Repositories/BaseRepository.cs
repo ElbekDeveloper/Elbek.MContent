e356f5a76bf9cba8a25c87ef8d2a4cfd9ce74b1f
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-14 11:48:40 +0500

    merge interface into base repository class

--- Elbek.MContent.DataAccess/Repositories/BaseRepository.cs
+++ Elbek.MContent.DataAccess/Repositories/BaseRepository.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 
 namespace Elbek.MContent.DataAccess.Repositories
 {
+    public interface IRepository<TModel> where TModel : BaseEntity
+    {
+        IQueryable<TModel> Query();
+        Task<TModel> GetByIdAsync(Guid id);
+        Task<IEnumerable<TModel>> GetAllAsync();
+        Task<TModel> AddAsync(TModel entity);
+        Task<TModel> UpdateAsync(TModel entity);
+        Task<TModel> DeleteAsync(TModel entity);
+    }
+
     public abstract class BaseRepository<TModel> : IRepository<TModel> where TModel : BaseEntity
     {
         protected readonly MContentContext _dbContext;
