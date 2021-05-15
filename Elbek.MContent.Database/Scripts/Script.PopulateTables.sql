ee12b045cf88ae7dae4db497723861752e6c6155
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-15 16:57:35 +0500

    Update structure of post deploye scripts

---
+++ Elbek.MContent.Database/Scripts/Script.PopulateTables.sql
@@ -0,0 +1,26 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the post-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+/*
+   Clear out the existing data
+*/
+
+-- TODO 1
+-- это файл с BuildAction = PostDeploy, но у тебя будет много разных постдеплой скриптов, 
+-- поэтому правильне будет вынести скрипты в отдельные sql файлы и эти файлы(скрипты) вызывать в этом файле.
+-- 1) создать папку Scrupts
+-- 2) создать в ней sql файл с кодом на для заполнения авторов.
+
+-- TODO 2
+-- скрипт заполнения авторов переделать в MERGE (create/update). Загугли MERGE script to fill initial data.
+-- можно сделать меньше данных, штук 10 хватит
+
