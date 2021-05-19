76bacd87888f1c54fc08c8988b1da4781d1e9bd5
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-19 13:37:22 +0500

    Add connections to content entity

--- Elbek.MContent.DataAccess/Data/Author.cs
+++ Elbek.MContent.DataAccess/Data/Author.cs
@@ -1,7 +1,14 @@
-﻿namespace Elbek.MContent.DataAccess.Data
+﻿using System.Collections.Generic;
+
+namespace Elbek.MContent.DataAccess.Data
 {
     public class Author : BaseEntity
     {
         public string Name { get; set; }
+        public virtual ICollection<AuthorContent> AuthorContents { get; set; }
+        public Author()
+        {
+            this.AuthorContents = new HashSet<AuthorContent>();
+        }
     }
 }
