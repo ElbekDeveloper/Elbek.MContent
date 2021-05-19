619ac8c4dc9ee6b5d194221cadb1f042ba059c68
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-19 13:37:35 +0500

    Add remaining entities

---
+++ Elbek.MContent.DataAccess/Data/AuthorContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Elbek.MContent.DataAccess.Data
+{
+    public class AuthorContent
+    {
+        public Guid Id { get; set; }
+        public Guid AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        public Guid ContentId { get; set; }
+        public virtual Content Content { get; set; }
+    }
+}
