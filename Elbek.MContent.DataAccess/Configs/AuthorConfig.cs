99e6cb4fcc1ec3a2686eeeed99905c881feb2876
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-19 13:37:01 +0500

    Write individual configurations

---
+++ Elbek.MContent.DataAccess/Configs/AuthorConfig.cs
@@ -0,0 +1,24 @@
+﻿using Elbek.MContent.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Elbek.MContent.DataAccess.Configs
+{
+    public class AuthorConfig : IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> modelBuilder)
+        {
+            modelBuilder.Property(a => a.Id)
+                 .HasColumnName("id")
+                  .HasMaxLength(36)
+                 .HasColumnType("varchar")
+                 .IsRequired(true);
+
+            modelBuilder.Property(a => a.Name)
+                .HasColumnName("Name")
+                .HasMaxLength(50)
+                .HasColumnType("nvarchar")
+                .IsRequired(true);
+        }
+    }
+}
---
+++ Elbek.MContent.DataAccess/Configs/AuthorContentConfig.cs
@@ -0,0 +1,32 @@
+﻿using Elbek.MContent.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Elbek.MContent.DataAccess.Configs
+{
+    public class AuthorContentConfig : IEntityTypeConfiguration<AuthorContent>
+    {
+        public void Configure(EntityTypeBuilder<AuthorContent> modelBuilder)
+        {
+            modelBuilder.Property(ac => ac.Id)
+                 .HasColumnName("id")
+                 .HasColumnType("UNIQUEIDENTIFIER");
+
+            modelBuilder.Property(ac => ac.AuthorId)
+                 .HasColumnName("AuthorId")
+                 .HasColumnType("UNIQUEIDENTIFIER");
+
+            modelBuilder.Property(ac => ac.ContentId)
+                 .HasColumnName("ContentId")
+                 .HasColumnType("UNIQUEIDENTIFIER");
+
+            modelBuilder.HasOne(a => a.Author)
+                                .WithMany(ac => ac.AuthorContents)
+                                .HasForeignKey(a => a.AuthorId);
+
+            modelBuilder.HasOne(c => c.Content)
+                                .WithMany(ac => ac.ContentAuthors)
+                                .HasForeignKey(c => c.ContentId);
+        }
+    }
+}
