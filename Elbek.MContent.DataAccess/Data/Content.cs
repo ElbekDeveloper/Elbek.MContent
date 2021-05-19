49b2d5eae74cfdf132d36a7b405e0825c0b2f126
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-19 16:24:55 +0500

    Add contents entity

--- Elbek.MContent.DataAccess/Data/Content.cs
+++ Elbek.MContent.DataAccess/Data/Content.cs
@@ -8,10 +8,10 @@ namespace Elbek.MContent.DataAccess.Data
         public Guid Id { get; set; }
         public string Title { get; set; }
         public Type Type { get; set; }
-        public virtual ICollection<AuthorContent> ContentAuthors { get; set; }
+        public virtual ICollection<ContentAuthors> ContentAuthors { get; set; }
         public Content()
         {
-            this.ContentAuthors = new HashSet<AuthorContent>();
+            this.ContentAuthors = new HashSet<ContentAuthors>();
         }
     }
 
