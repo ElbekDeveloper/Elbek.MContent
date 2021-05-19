3a013d06cf6ccf17f1d66e2406230c7ed8c9ed51
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-19 12:29:15 +0500

    AC 1 and AC 1.1  Create contents tables

---
+++ Elbek.MContent.Database/dbo/Tables/ContentAuthors.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ContentAuthors]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
+	[AuthorId] UNIQUEIDENTIFIER NOT NULL,
+	[ContentId] UNIQUEIDENTIFIER NOT NULL,
+	FOREIGN KEY ([AuthorId]) REFERENCES [dbo].[Authors],
+	FOREIGN KEY ([ContentId]) REFERENCES [dbo].[Contents]
+	)
