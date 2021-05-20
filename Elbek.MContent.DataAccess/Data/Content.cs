7975710ee11cca5349b99e917dde10a26890d4b3
n.elbek@outlook.com <n.elbek@outlook.com>
2021-05-20 10:11:23 +0500

    TODO 1 enum вынести в отдельный класс ContentType

--- Elbek.MContent.DataAccess/Data/Content.cs
+++ Elbek.MContent.DataAccess/Data/Content.cs
@@ -7,21 +7,12 @@ namespace Elbek.MContent.DataAccess.Data
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public Type Type { get; set; }
+        public ContentType Type { get; set; }
         public virtual ICollection<ContentAuthors> ContentAuthors { get; set; }
 
         public Content()
         {
             this.ContentAuthors = new HashSet<ContentAuthors>();
         }
-    }
-
-    /// TODO 1 enum вынести в отдельный класс ContentType
-    public enum Type
-    {
-        Book,
-        Song,
-        Movie,
-        Podcast
     }
 }
