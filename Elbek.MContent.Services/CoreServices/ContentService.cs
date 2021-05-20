39f9a417c73e47973084e86a019fbea0dc74da80
ElbekDeveloper <e.normurodov@mail.ru>
2021-05-20 11:53:40 +0500

    Create content base service

---
+++ Elbek.MContent.Services/CoreServices/ContentService.cs
@@ -0,0 +1,36 @@
+﻿using Elbek.MContent.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elbek.MContent.Services.CoreServices
+{
+    public interface IContentService : ICoreService<ContentDto>
+    {
+
+    }
+    public class ContentService : IContentService
+    {
+        public Task<MContentResult<ContentDto>> AddAsync(ContentDto TModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MContentResult<IList<ContentDto>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MContentResult<ContentDto>> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MContentResult<ContentDto>> UpdateAsync(Guid id, ContentDto TModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
