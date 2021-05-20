0953ee35d0c47bd5387fffb85941b3605a2f9768
ElbekDeveloper <e.normurodov@mail.ru>
2021-05-20 16:10:28 +0500

    Refactor validation implementation with more abstraction

--- Elbek.MContent.Services/ValidationServices/AuthorValidator/AuthorValidationRules.cs
+++ Elbek.MContent.Services/ValidationServices/AuthorValidator/AuthorValidationRules.cs
@@ -1,11 +1,8 @@
 ﻿using Elbek.MContent.DataAccess.Data;
-using Elbek.MContent.Services.Models;
 using System;
-using System.Threading.Tasks;
 
 namespace Elbek.MContent.Services.ValidationServices.AuthorValidator
 {
-    //https://github.com/aspnet/samples/blob/main/samples/aspnet/WebApi/ValidationSample/ValidationSample/Validators/NonNegativeAttribute.cs
     public interface IAuthorValidationRules : IGenericValidationRules
     {
         string ValidateAuthorWasFound(Guid id, Author author);
