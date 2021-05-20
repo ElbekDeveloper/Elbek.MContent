2a962a28bc954eb0b1349db93f050e6d53a7ffcb
ElbekDeveloper <e.normurodov@mail.ru>
2021-05-20 16:32:52 +0500

    Create content service

--- Elbek.MContent.Services/CoreServices/ContentService.cs
+++ Elbek.MContent.Services/CoreServices/ContentService.cs
@@ -1,4 +1,8 @@
-﻿using Elbek.MContent.Services.Models;
+﻿using AutoMapper;
+using Elbek.MContent.DataAccess.Repositories;
+using Elbek.MContent.Services.Extensions;
+using Elbek.MContent.Services.Models;
+using Elbek.MContent.Services.ValidationServices.ContentValidator;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +17,39 @@ namespace Elbek.MContent.Services.CoreServices
     }
     public class ContentService : IContentService
     {
+        private readonly IContentRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly IContentValidationService _validationService;
+        public MContentResult<ContentDto> ResultDto { get; set; } = new MContentResult<ContentDto>();
+        public MContentResult<IList<ContentDto>> ResultListDto { get; set; } = new MContentResult<IList<ContentDto>>();
+
+        public ContentService(IContentRepository repository, IMapper mapper, IContentValidationService validationService)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _validationService = validationService;
+        }
+
         public Task<MContentResult<ContentDto>> AddAsync(ContentDto TModel)
         {
             throw new NotImplementedException();
         }
 
-        public Task<MContentResult<IList<ContentDto>>> GetAllAsync()
+        public async Task<MContentResult<IList<ContentDto>>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var validationResult = await _validationService.ValidateGetAll();
+
+            if (!validationResult.IsValid)
+            {
+                return validationResult.ConvertFromValidationResult<IList<ContentDto>>();
+            }
+
+            var contents = await _repository.GetAllAsync();
+            var contentDtos = _mapper.Map<IList<ContentDto>>(contents);
+
+            ResultListDto.Data = contentDtos;
+            ResultListDto.StatusCode = (int)StatusCodes.Ok;
+            return ResultListDto;
         }
 
         public Task<MContentResult<ContentDto>> GetByIdAsync(Guid id)
