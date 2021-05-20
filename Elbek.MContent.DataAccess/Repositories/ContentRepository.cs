930e8d1ad18321cbe2979a622a282f2706c9683a
ElbekDeveloper <e.normurodov@mail.ru>
2021-05-20 11:46:01 +0500

    Create content repo

---
+++ Elbek.MContent.DataAccess/Repositories/ContentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Elbek.MContent.DataAccess.Data;
+
+namespace Elbek.MContent.DataAccess.Repositories
+{
+    public interface IContentRepository: IRepository<Content>
+    {
+
+    }
+    public class ContentRepository : BaseRepository<Content>, IContentRepository
+    {
+        public ContentRepository(MContentContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
